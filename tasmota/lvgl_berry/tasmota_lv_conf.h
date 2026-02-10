3fd97b97b0ad5af9ccdbbf99b9a918fac7bb074b
Martin Macák <macak.nwa@gmail.com>
2026-02-10 17:00:25 +0100

    LVGL Performance Update

--- tasmota/lvgl_berry/tasmota_lv_conf.h
+++ tasmota/lvgl_berry/tasmota_lv_conf.h
@@ -91,7 +91,7 @@
  *====================*/
 
 /** Default display refresh, input device read and animation step period. */
-#define LV_DEF_REFR_PERIOD  50      /**< [ms] */
+#define LV_DEF_REFR_PERIOD  40      /**< [ms] */
 
 /** Default Dots Per Inch. Used to initialize default sizes such as widgets sized, style paddings.
  * (Not so important, you can adjust it to modify default sizes and spaces.) */
@@ -146,7 +146,7 @@
  * and can't be drawn in chunks. */
 
 /** The target buffer size for simple layer chunks. */
-#define LV_DRAW_LAYER_SIMPLE_BUF_SIZE    (1024 * 1024)    /**< [bytes]*/
+#define LV_DRAW_LAYER_SIMPLE_BUF_SIZE    (4096 * 1024)    /**< [bytes]*/
 
 /* Limit the max allocated memory for simple and transformed layers.
  * It should be at least `LV_DRAW_LAYER_SIMPLE_BUF_SIZE` sized but if transformed layers are also used
@@ -157,7 +157,7 @@
 /** Stack size of drawing thread.
  * NOTE: If FreeType or ThorVG is enabled, it is recommended to set it to 32KB or more.
  */
-#define LV_DRAW_THREAD_STACK_SIZE    (16 * 1024)         /**< [bytes]*/
+#define LV_DRAW_THREAD_STACK_SIZE    (128 * 1024)         /**< [bytes]*/
 
 /** Thread priority of the drawing task.
  *  Higher values mean higher priority.
@@ -178,14 +178,14 @@
      */
     #define LV_DRAW_SW_SUPPORT_RGB565       1
     #define LV_DRAW_SW_SUPPORT_RGB565_SWAPPED       0   // TASMOTA
-    #define LV_DRAW_SW_SUPPORT_RGB565A8     0       // TASMOTA
+    #define LV_DRAW_SW_SUPPORT_RGB565A8     1       // TASMOTA
     #define LV_DRAW_SW_SUPPORT_RGB888       1
     #define LV_DRAW_SW_SUPPORT_XRGB8888     0       // TASMOTA
     #define LV_DRAW_SW_SUPPORT_ARGB8888     1
-    #define LV_DRAW_SW_SUPPORT_ARGB8888_PREMULTIPLIED 0     // TASMOTA
-    #define LV_DRAW_SW_SUPPORT_L8           0       // TASMOTA
+    #define LV_DRAW_SW_SUPPORT_ARGB8888_PREMULTIPLIED 1     // TASMOTA
+    #define LV_DRAW_SW_SUPPORT_L8           1       // TASMOTA
     #define LV_DRAW_SW_SUPPORT_AL88         1
-    #define LV_DRAW_SW_SUPPORT_A8           0       // TASMOTA
+    #define LV_DRAW_SW_SUPPORT_A8           1       // TASMOTA
     #define LV_DRAW_SW_SUPPORT_I1           1
 
     /* The threshold of the luminance to consider a pixel as
@@ -500,7 +500,7 @@
  *  If size is not set to 0, the decoder will fail to decode when the cache is full.
  *  If size is 0, the cache function is not enabled and the decoded memory will be
  *  released immediately after use. */
-#define LV_IMG_CACHE_DEF_SIZE_PSRAM     (4096*1024)     // TASMOTA - still used?
+#define LV_IMG_CACHE_DEF_SIZE_PSRAM     (8192*1024)     // TASMOTA - still used?
 #define LV_IMG_CACHE_DEF_SIZE_NOPSRAM   (32*1024)       // TASMOTA - still used?
 #define LV_CACHE_DEF_SIZE               1024            // TASMOTA very low value as a place-holder, the true value is adjusted later
 
@@ -805,7 +805,7 @@
 
 #define LV_USE_CALENDAR   1
 #if LV_USE_CALENDAR
-    #define LV_CALENDAR_WEEK_STARTS_MONDAY 0
+    #define LV_CALENDAR_WEEK_STARTS_MONDAY 1
     #if LV_CALENDAR_WEEK_STARTS_MONDAY
         #define LV_CALENDAR_DEFAULT_DAY_NAMES {"Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"}
     #else
@@ -906,7 +906,7 @@
 #define LV_USE_THEME_SIMPLE 1
 
 /** A theme designed for monochrome displays */
-#define LV_USE_THEME_MONO 1
+#define LV_USE_THEME_MONO 0
 
 /*==================
  * LAYOUTS
@@ -1048,7 +1048,7 @@
 
     /** Cache count of glyphs in FreeType, i.e. number of glyphs that can be cached.
      *  The higher the value, the more memory will be used. */
-    #define LV_FREETYPE_CACHE_FT_GLYPH_CNT 512
+    #define LV_FREETYPE_CACHE_FT_GLYPH_CNT 256
 #endif
 
 /** Built-in TTF decoder */
