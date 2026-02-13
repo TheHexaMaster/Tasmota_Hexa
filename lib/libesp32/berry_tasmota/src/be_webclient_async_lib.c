c3a5427eb52654737435c5c26fa185ef7ce971e4
Martin Macák <macak.nwa@gmail.com>
2026-02-13 23:48:25 +0100

    Webclient Async update 2

--- lib/libesp32/berry_tasmota/src/be_webclient_async_lib.c
+++ lib/libesp32/berry_tasmota/src/be_webclient_async_lib.c
@@ -1,10 +1,12 @@
 /********************************************************************
- * Webclient mapped to Arduino framework
+ * Webclient ASYNC mapped to Arduino framework
+ * 
+ * 2026 - Update to use with ASYNC Webclient by HexaMaster
  *
  * To use: `d = webclient()`
  *
  *******************************************************************/
-#ifndef USE_BERRY_WEBCLIENT_ASYNC
+#ifdef USE_BERRY_WEBCLIENT_ASYNC
 
 #include "be_constobj.h"
 
@@ -32,21 +34,51 @@ extern int wc_writeflash(bvm *vm);
 extern int wc_getsize(bvm *vm);
 extern int wc_getbytes(bvm *vm);
 
-#include "be_fixed_be_class_webclient.h"
+/* --- TLS pinning extensions --- */
+extern int wc_tls_pin_pubkey(bvm *vm);
+extern int wc_tls_clear_pins(bvm *vm);
+extern int wc_tls_set_rsa_only(bvm *vm);
+/* ------------------------------- */
+
+/* --- Async extensions --- */
+extern int wc_async_get_start(bvm *vm);
+extern int wc_async_post_start(bvm *vm);   /* payload: string|bytes */
+extern int wc_async_state(bvm *vm);
+extern int wc_async_abort(bvm *vm);
+/* ------------------------ */
+
+
+#include "be_fixed_be_class_webclient_async.h"
 
 void be_load_webclient_lib(bvm *vm) {
-    be_pushntvclass(vm, &be_class_webclient);
+    be_pushntvclass(vm, &be_class_webclient_async);
     be_setglobal(vm, "webclient");
     be_pop(vm, 1);
 }
 /* @const_object_info_begin
 
-class be_class_webclient (scope: global, name: webclient) {
+class be_class_webclient_async (scope: global, name: webclient) {
     .p, var
     .w, var
+
+    // async native layer
+    ._last_code, var
+    ._last_rx, var
+    .__async_hold, var
+
+    // public mirrors async
+    last_code, var
+    last_rx, var
+
     init, func(wc_init)
     deinit, func(wc_deinit)
     url_encode, static_func(wc_urlencode)
+
+    // TLS pinning API
+    tls_pin_pubkey, func(wc_tls_pin_pubkey)
+    tls_clear_pins, func(wc_tls_clear_pins)
+    tls_set_rsa_only, func(wc_tls_set_rsa_only)
+
 
     begin, func(wc_begin)
     set_timeouts, func(wc_set_timeouts)
@@ -59,6 +91,8 @@ class be_class_webclient (scope: global, name: webclient) {
     get_header, func(wc_get_header)
 
     close, func(wc_close)
+    // header helpers (both spellings kept for compatibility)
+    addheader, func(wc_addheader)
     add_header, func(wc_addheader)
     GET, func(wc_GET)
     POST, func(wc_POST)
@@ -70,6 +104,12 @@ class be_class_webclient (scope: global, name: webclient) {
     write_flash, func(wc_writeflash)
     get_size, func(wc_getsize)
     get_bytes, func(wc_getbytes)
+
+    // --- async controls ---
+    async_get_start, func(wc_async_get_start)
+    async_post_start, func(wc_async_post_start)
+    async_state, func(wc_async_state)
+    async_abort, func(wc_async_abort)
 }
 @const_object_info_end */
 #endif
