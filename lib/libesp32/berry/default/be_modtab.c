e13237519bdc6910da7c423793d644f85e861afc
Martin Macák <macak.nwa@gmail.com>
2026-03-06 23:58:04 +0100

    Added berry MQTT client

--- lib/libesp32/berry/default/be_modtab.c
+++ lib/libesp32/berry/default/be_modtab.c
@@ -245,6 +245,9 @@ be_extern_native_class(modbus_rtu);
 be_extern_native_class(webclient);
 be_extern_native_class(tcpclient);
 be_extern_native_class(tcpclientasync);
+#ifdef USE_BERRY_MQTTCLIENT
+be_extern_native_class(mqttclient);
+#endif // USE_BERRY_MQTTCLIENT
 be_extern_native_class(webserver_async);
 be_extern_native_class(tcpserver);
 be_extern_native_class(energy_struct);
@@ -299,6 +302,9 @@ BERRY_LOCAL bclass_array be_class_table = {
     &be_native_class(webclient),
     &be_native_class(tcpclient),
     &be_native_class(tcpclientasync),
+#ifdef USE_BERRY_MQTTCLIENT
+    &be_native_class(mqttclient),
+#endif // USE_BERRY_MQTTCLIENT
 #ifdef USE_BERRY_DEBUG
     &be_native_class(webserver_async),  // include only when USE_BERRY_DEBUG is enabled
 #endif // USE_BERRY_DEBUG
